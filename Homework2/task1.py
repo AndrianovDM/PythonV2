28c88872ec7db1628b8331561a2f8177d237f247
Andrianov Dmitry <andrianovdm1997@mail.ru>
2023-04-25 05:49:32 +0300

    task1

--- Homework2/task1.py
+++ Homework2/task1.py
@@ -1,22 +1,36 @@
 # Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление. 
 # Функцию hex используйте для проверки своего результата.
 
-def convert(a: int , base: int) -> str :
+def convert(num: int, base: int ) -> str:
     str_rep = ''
-    while a:
-        str_rep = str(a % base) + str_rep
-        a //= base
+    while num:
+        if (num % base) < 10:
+            temp = num % base
+        else:
+            temp = chr(num % base + 87)
+
+        str_rep = str(temp) + str_rep
+        num //= base
+
+    if base == 2:
+        str_rep = "0b" + str_rep
+    elif base == 8:
+        str_rep = "0o" + str_rep
+    elif base == 16:
+        str_rep = "0x" + str_rep
+
     return str_rep
 
-def convert(number: int, mod: int = 16) -> str:
-    result = ''
-    while number != 0:
-        temp = number % mod if (number % mod) < 10 else chr(number % mod + 87)
-        result = str(temp) + result
-        number //= mod
-    result = "0x" + result
-    return result
+number = int(input('Введите число: '))
+systeam = int(input('Ввидите тип системы исчисления: '))
 
+if systeam == 2:
+    print('Результат:', convert(number, systeam))
+    print('Проверка:', bin(number))
+elif systeam == 8:
+    print('Результат:', convert(number, systeam))
+    print('Проверка:', bin(number))
 
-print(convert(10, 8))
-print(hex(40))
+elif systeam == 16:
+    print('Результат:', convert(number, systeam))
+    print('Проверка:', bin(number))
